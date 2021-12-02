f0c8fdf9bfab8782dd0ac66bcd3b9a5fdfed35a9
Zihao Zhang <zihaozhang@ucsb.edu>
2021-12-01 21:09:26 -0800

    updates

--- bionic_eye_device.py
+++ bionic_eye_device.py
@@ -4,15 +4,14 @@ import sys
 
 class Device:
     # This class simulate the bionic eye device
-    def __init__(self, img_path, src_path, dst_path, delay, address, module):
-        self.img_path = img_path
-        self.src_path = src_path
+    def __init__(self, repo_path, dst_path, delay, address, module):
+        self.repo_path = repo_path
         self.dst_path = dst_path
         self.address = address
         self.module = module
         self.processor = Processor()
-        self.camera = Camera(img_path, src_path)
-        self.network = Network(self.src_path, self.dst_path, delay = 0.0)
+        self.camera = Camera(repo_path)
+        self.network = Network(repo_path, dst_path, delay = 0.0)
     
     def take_imgs(self):
         # Take imgs and store them in the src_folder
@@ -25,7 +24,7 @@ class Device:
     def load_info(self):
         # Write address and module into client_info.txt in the src_folder
         f = open('src_folder/client_info.txt', 'w')
-        f.write(self.address + '\n')
+        f.write(self.address + ':' + repo_path + '\n')
         f.write(self.module + '\n')
         f.close()
     
@@ -38,26 +37,26 @@ class Camera:
     # This class simulate the camera that takes photos
     # Phase 1: read image from a destination
 
-    def __init__(self, img_path, src_path):
-        self.img_path = img_path
-        self.src_path = src_path
+    def __init__(self, repo_path):
+        self.img_path = repo_path + '/img_folder'
+        self.src_path = repo_path + '/src_folder'
         
     def take_imgs(self):
         # move images from img_path to src_path
         valid_ext = ['.jpeg', '.jpg','.gif','.png','.tga']
-        for f in os.listdir(img_path):
+        for f in os.listdir(self.img_path):
             ext = os.path.splitext(f)[1]
             if ext.lower() not in valid_ext:
                 continue
-            shutil.copy(img_path + '/' + f, src_path)
+            shutil.copy(self.img_path + '/' + f, src_path)
 
 class Network:
     cmd = 'rclone copy'
     flags = ''
 
     # This class simulate the network with linux util rsync
-    def __init__(self, src_path, dst_path, delay = 0.0):
-        self.src_path = src_path
+    def __init__(self, repo_path, dst_path, delay = 0.0):
+        self.src_path = repo_path + '/src_folder'
         self.dst_path = dst_path
         self.delay = delay
         
@@ -74,18 +73,15 @@ class Network:
 
 if __name__ == '__main__':
     repo_path = sys.argv[1]
-    img_path = repo_path + '/img_folder'
-    src_path = repo_path + '/src_folder'
-    res_path = repo_path + '/res_folder'
     dst_path_remote = 'Bionic_Eye_IoT_Server:Input'
 
-    cmd = 'mkdir ' + src_path + ' ; ' + 'mkdir ' + res_path
+    cmd = 'mkdir ' + repo_path + '/src_folder'
     os.system(cmd)
 
     client_address = sys.argv[2]
     module_selected = sys.argv[3]
     
-    device = Device(img_path, src_path, dst_path_remote,
+    device = Device(repo_path, dst_path_remote,
                     0.0, client_address, module_selected)
     device.load_info()
     device.take_imgs()
